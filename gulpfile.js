134fbd4e1b37717c3fb28724b7e823f84e037724
spacemonaut <4585677+spacemonaut@users.noreply.github.com>
2018-11-25 18:07:03 +0000

    initial framework

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var del = require('del');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'js', 'html'], function() {
+
+  browserSync.init({
+    server: './dist'
+  });
+
+  gulp.watch('src/scss/*.scss', ['sass']);
+  gulp.watch('src/js/*.js', ['js']);
+  gulp.watch('src/*.html', ['html']).on('change', browserSync.reload);
+});
+
+gulp.task('js', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('html', function() {
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src('src/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
