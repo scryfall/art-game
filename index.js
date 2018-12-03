69afac4e2a698518254b6142d75881d66d516974
spacemonaut <4585677+spacemonaut@users.noreply.github.com>
2018-12-03 22:54:33 +0000

    update security policy

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+const ENV_DEV = 'development'
+const ENV_PROD = 'production';
+const ENV = process.env.NODE_ENV || ENV_DEV;
+
+const CONTENT_SECURITY_POLICY = [
+  "default-src *.scryfall.com",
+  "script-src *.scryfall.com 'unsafe-eval' 'unsafe-inline'",
+  "style-src *.scryfall.com 'unsafe-inline'",
+  "img-src *.scryfall.com 'unsafe-inline' data:",
+  "block-all-mixed-content"
+].join('; ');
 
 express()
   .use(express.static(path.join(__dirname, 'dist'), {
     setHeaders: (res, path, stat) => {
-      res.set('Content-Security-Policy-Report-Only', "default-src 'self' *.scryfall.com scryfall-art-game.herokuapp.com")
+      let policyHeader = 'Content-Security-Policy';
+      if (ENV === ENV_DEV) {
+        policyHeader = 'Content-Security-Policy-Report-Only';
+      }
+      res.set(policyHeader, CONTENT_SECURITY_POLICY)
       res.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
       res.set('Referrer-Policy', 'strict-origin-when-cross-origin');
       res.set('X-Frame-Options', 'DENY');
