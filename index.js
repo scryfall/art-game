22f799550e83d809c6c774481e4ac443a469760a
spacemonaut <4585677+spacemonaut@users.noreply.github.com>
2018-12-03 22:20:27 +0000

    add security headers

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 express()
-  .use(express.static(path.join(__dirname, 'dist')))
+  .use(express.static(path.join(__dirname, 'dist'), {
+    setHeaders: (res, path, stat) => {
+      res.set('Content-Security-Policy', "default-src 'self' *.scryfall.com")
+      res.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+      res.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+      res.set('X-Frame-Options', 'DENY');
+      res.set('X-Permitted-Cross-Domain-Policies', 'none');
+      res.set('X-XSS-Protection', '1; mode=block');
+      res.set('X-Download-Options', 'noopen');
+    }
+  }))
   .listen(PORT, () => console.log(`Listening on port ${PORT} at directory ${__dirname}`));
