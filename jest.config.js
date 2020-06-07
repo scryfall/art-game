247445b2c8b193db5d839cd761e45412d01b9772
spacemonaut <4585677+spacemonaut@users.noreply.github.com>
2020-06-07 14:32:01 +0100

    Bugfix/remove jest (#50)
    * remove jest conf and snippets
    
    * uninstall jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
-export default {
-  moduleNameMapper: {
-    "^vue$": 'vue/dist/vue.js'
-  },
-  testEnvironment: "jest-environment-jsdom-sixteen",
-};
