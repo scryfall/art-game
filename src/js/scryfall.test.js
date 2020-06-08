7c97c2b3d388c417ba490dfc6edea7e7e958edb0
Blade Barringer <blade@crookedneighbor.com>
2020-06-08 14:12:51 -0500

    Fix jest mock

--- src/js/scryfall.test.js
+++ src/js/scryfall.test.js
@@ -11,7 +11,7 @@ describe('Scryfall', () => {
     const data = { name: 'test name' };
 
     beforeEach(() => {
-      scryfall.axios.get = jest.mockResolvedValue({ data });
+      scryfall.axios.get = jest.fn().mockResolvedValue({ data });
     });
 
     it('should call with the requested format', async () => {
