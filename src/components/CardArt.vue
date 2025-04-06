e9f727a0fa9f686b5862168a28e13b7e8d3a8b26
scarletcs <4585677+scarletcs@users.noreply.github.com>
2025-04-06 23:15:56 +0100

    nicer card loading

--- src/components/CardArt.vue
+++ src/components/CardArt.vue
@@ -1,15 +1,50 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref, watch } from "vue";
 import type { ScryfallCard } from "../models/scryfall-card";
+import { LoadingStatus } from "../store";
+import Pulser from "./Pulser.vue";
 
-const props = defineProps<{ card: ScryfallCard }>();
+const props = defineProps<{ card: ScryfallCard; loadingNext: LoadingStatus }>();
 const imageUri = computed(() => props.card.image_uris.art_crop);
+
+const status = ref(LoadingStatus.Pending);
+
+watch(imageUri, () => {
+  status.value = LoadingStatus.Pending;
+});
+
+const onLoad = () => {
+  status.value = LoadingStatus.Success;
+};
+
+const onError = () => {
+  status.value = LoadingStatus.Failed;
+};
+
+const showLoadingOverlay = computed(() => {
+  return props.loadingNext === LoadingStatus.Pending || status.value === LoadingStatus.Pending;
+});
+
+const showErrorOverlay = computed(() => {
+  return props.loadingNext === LoadingStatus.Failed || status.value === LoadingStatus.Failed;
+});
 </script>
 
 <template>
   <div class="art-frame">
-    <!-- TODO(#35) Use this preloader to determine that the image is still loading. -->
-    <!-- <img alt="" class="vh preload" :src="card.image_uris.art_crop" /> -->
+    <div v-if="showErrorOverlay" class="overlay error">
+      <div class="cross"></div>
+    </div>
+    <div v-else-if="showLoadingOverlay" class="overlay loading">
+      <Pulser class="pulser" />
+    </div>
+    <img
+      alt=""
+      class="vh preload"
+      :src="card.image_uris.art_crop"
+      :onload="onLoad"
+      :onerror="onError"
+    />
     <img :src="imageUri" />
   </div>
 </template>
@@ -19,9 +54,59 @@ $default-art-height: 456px;
 $default-art-width: 626px;
 
 .art-frame {
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
+}
+
+.pulser {
+  opacity: 0.5;
+}
+
+.cross {
+  --size: 80px;
+  position: relative;
+  font-size: var(--size);
+  width: 1em;
+  height: 1em;
+  color: var(--c-salmon);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &::before,
+  &::after {
+    content: "";
+    background: currentColor;
+    display: block;
+    height: calc(1em * 1.25);
+    width: 0.2em;
+    border-radius: 0.1em;
+
+    position: absolute;
+  }
+
+  &::before {
+    transform: rotate(-45deg);
+  }
+
+  &::after {
+    transform: rotate(45deg);
+  }
+}
+
+.overlay {
+  position: absolute;
+  inset: 0;
+  z-index: 1;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: rgba(0 0 0 / 0.5);
 }
 
 img {
---
+++ src/components/Pulser.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="loader"></div>
+</template>
+
+<style scoped lang="scss">
+// Pulser courtesy of https://css-loaders.com/pulsing/
+.loader {
+  --mid-color: color-mix(in srgb, currentColor 75%, transparent);
+  --faded-color: color-mix(in srgb, currentColor 0%, transparent);
+
+  width: 20px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  background: currentColor;
+  box-shadow: 0 0 0 0 var(--mid-color);
+  animation: l2 1.5s infinite linear;
+  position: relative;
+}
+.loader:before,
+.loader:after {
+  content: "";
+  position: absolute;
+  inset: 0;
+  border-radius: inherit;
+  box-shadow: 0 0 0 0 var(--mid-color);
+  animation: inherit;
+  animation-delay: -0.5s;
+}
+.loader:after {
+  animation-delay: -1s;
+}
+@keyframes l2 {
+  100% {
+    box-shadow: 0 0 0 40px var(--faded-color);
+  }
+}
+</style>
