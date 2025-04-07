6e58b0ba9fbdf84500d5d783ac2bb87beb46bd66
scarletcs <4585677+scarletcs@users.noreply.github.com>
2025-04-07 15:08:13 +0100

    nicer theme toggle

--- src/components/ThemeButton.vue
+++ src/components/ThemeButton.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { Theme } from "../models/theme";
 import { useAppDispatch, useAppSelector, setTheme } from "../store";
-import { capitalize } from "../utils/string";
+import MoonSolidSvg from "./Svg/MoonSolidSvg.vue";
+import SunOutlineSvg from "./Svg/SunOutlineSvg.vue";
 
 const dispatch = useAppDispatch();
 const theme = useAppSelector((state) => state.config.theme);
@@ -15,9 +16,69 @@ const toggle = () => {
 </script>
 
 <template>
-  <button type="button" class="btn btn-small" @click="toggle" @keypress.enter="toggle">
-    {{ capitalize(theme) }} theme
+  <button
+    type="button"
+    :class="{ light: theme === Theme.Light, dark: theme === Theme.Dark }"
+    @click="toggle"
+    @keypress.enter="toggle"
+  >
+    <div class="toggle"></div>
+    <div class="icon sun">
+      <SunOutlineSvg />
+    </div>
+    <div class="icon moon">
+      <MoonSolidSvg />
+    </div>
   </button>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+button {
+  --color: currentColor;
+  --inverted: var(--page-background);
+  --size: 28px;
+  --halfsize: calc(var(--size) / 2);
+
+  position: relative;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 8px;
+  border-radius: var(--halfsize);
+  height: var(--size);
+  font-size: 20px;
+  border-width: 0;
+  background-color: var(--color);
+}
+
+.icon {
+  display: flex;
+}
+
+.icon {
+  z-index: 2;
+}
+
+.light .moon {
+  color: var(--inverted);
+}
+
+.dark .sun {
+  color: var(--inverted);
+}
+
+.toggle {
+  position: absolute;
+  width: 24px;
+  height: 24px;
+  border-radius: 12px;
+  background-color: var(--inverted);
+  left: 2px;
+  transition: left 0.2s ease-out;
+}
+
+.dark .toggle {
+  left: 30px;
+}
+</style>
