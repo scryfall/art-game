608e9ef7fc2e6b79788c71ad8b63f6ffef7bfe68
Blade Barringer <blade@crookedneighbor.com>
2025-04-15 22:35:08 -0500

    feat: add 404 page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// TODO - maybe randomize from various options?
+const imgSrc = "/lost.jpg";
+</script>
+
+<template>
+  <section>
+    <h1>404 Not Found</h1>
+    <img :src="imgSrc" alt="" />
+  </section>
+</template>
+
+<style scoped lang="scss">
+section {
+  text-align: center;
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,12 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import ScreenManager from "./pages/ScreenManager.vue";
 import SettingsScreen from "./pages/SettingsScreen.vue";
+import NotFound from "./pages/NotFound.vue";
 
 const routes = [
   { path: "/", component: ScreenManager },
   { path: "/settings", component: SettingsScreen },
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
 ];
 
 const router = createRouter({
