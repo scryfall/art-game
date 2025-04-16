7f7b368aaf60def88db98040c626d5e39f84d48d
Scarlett <4585677+scarletcs@users.noreply.github.com>
2025-04-16 01:15:48 +0100

    Update src/utils/timer.ts
    Co-authored-by: Blade Barringer <blade@crookedneighbor.com>

--- src/utils/timer.ts
+++ src/utils/timer.ts
@@ -3,5 +3,5 @@
  * @param ms The milliseconds to wait
  */
 export function wait(ms: number) {
-  return new Promise<void>((resolve) => setTimeout(() => resolve(), ms));
+  return new Promise<void>((resolve) => setTimeout(resolve, ms));
 }
