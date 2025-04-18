fd5a66900a24c8315408ed5b8ebd22f07ae92af1
scarletcs <4585677+scarletcs@users.noreply.github.com>
2025-04-18 19:31:46 +0100

    http specs

--- src/utils/__test__/http.test.ts
+++ src/utils/__test__/http.test.ts
@@ -1,26 +1,42 @@
 import type { Mock } from "vitest";
-import { Http } from "../http";
+import { Http, HttpError } from "../http";
 
 describe("http", () => {
+  let http: Http;
+  let fetchSpy: Mock;
+
+  beforeEach(() => {
+    fetchSpy = vi.fn();
+    http = new Http();
+  });
+
   describe("fetch", () => {
-    let fetchSpy: Mock;
+    const body = { data: "foo" };
 
     beforeEach(() => {
-      fetchSpy = vi.fn().mockResolvedValue({
-        json: vi.fn().mockResolvedValue({ data: "foo" }),
+      fetchSpy.mockResolvedValue({
+        status: 200,
+        ok: true,
+        json: vi.fn().mockResolvedValue(body),
       });
       vi.stubGlobal("fetch", fetchSpy);
     });
 
     it("calls fetch with url", async () => {
-      const http = new Http();
-
       const res = await http.fetch("https://example.com/foo");
 
-      expect(res).toEqual({
-        data: "foo",
-      });
+      expect(res).toEqual(body);
       expect(fetchSpy).toBeCalledWith("https://example.com/foo");
+    });
+
+    it("throws if response was not OK", async () => {
+      fetchSpy.mockResolvedValue({
+        status: 400,
+        ok: false,
+        json: vi.fn().mockResolvedValue(body),
+      });
+
+      await expect(http.fetch("https://example.com/foo")).rejects.toThrow(HttpError);
     });
   });
 });
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,10 +1,21 @@
+export class HttpError extends Error {
+  constructor(public readonly response: Response, public readonly body: unknown) {
+    super("HTTP Request failed");
+  }
+
+  public get status() {
+    return this.response.status;
+  }
+}
+
 export class Http {
-  public async fetch<T>(url: string | URL) {
+  public async fetch<T>(url: string | URL): Promise<T> {
     const response = await fetch(url.toString());
+    const body = await response.json();
     if (response.ok) {
-      return response.json() as T;
+      return body;
     } else {
-      throw response.json();
+      throw new HttpError(response, body);
     }
   }
 }
