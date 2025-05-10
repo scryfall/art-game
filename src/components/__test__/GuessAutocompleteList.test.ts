6c60a892cb4b7067fd0497dd9267c9dfee57a154
Blade Barringer <blade@crookedneighbor.com>
2025-05-10 17:55:55 -0500

    chore: clarify tests

--- src/components/__test__/GuessAutocompleteList.test.ts
+++ src/components/__test__/GuessAutocompleteList.test.ts
@@ -20,10 +20,14 @@ describe("GuessAutoompleteList", () => {
     await rerender({
       options: ["one", "two", "three"],
     });
-    expect(screen.queryAllByRole("button")).toHaveLength(3);
+    const buttons = screen.queryAllByRole("button");
+    expect(buttons).toHaveLength(3);
+    expect(buttons[0]).toHaveTextContent("one");
+    expect(buttons[1]).toHaveTextContent("two");
+    expect(buttons[2]).toHaveTextContent("three");
   });
 
-  it("marks as active when keyboardFocusIndex is 0 or greater", async () => {
+  it("marks container as active when keyboardFocusIndex is 0 or greater", async () => {
     const { rerender } = render(GuessAutocompleteList, {
       props: {
         options: ["option"],
@@ -42,5 +46,40 @@ describe("GuessAutoompleteList", () => {
       keyboardFocusIndex: 1,
     });
     expect(screen.getByTestId("options-wrapper")).toHaveClass("active");
+  });
+
+  it("marks particular option as active when the keyboardFocusIndex matches", async () => {
+    const { rerender } = render(GuessAutocompleteList, {
+      props: {
+        options: ["one", "two", "three"],
+        keyboardFocusIndex: -1,
+      },
+    });
+
+    const options = screen.getAllByRole("button");
+    expect(options[0]).not.toHaveClass("active");
+    expect(options[1]).not.toHaveClass("active");
+    expect(options[2]).not.toHaveClass("active");
+
+    await rerender({
+      keyboardFocusIndex: 0,
+    });
+    expect(options[0]).toHaveClass("active");
+    expect(options[1]).not.toHaveClass("active");
+    expect(options[2]).not.toHaveClass("active");
+
+    await rerender({
+      keyboardFocusIndex: 1,
+    });
+    expect(options[0]).not.toHaveClass("active");
+    expect(options[1]).toHaveClass("active");
+    expect(options[2]).not.toHaveClass("active");
+
+    await rerender({
+      keyboardFocusIndex: 2,
+    });
+    expect(options[0]).not.toHaveClass("active");
+    expect(options[1]).not.toHaveClass("active");
+    expect(options[2]).toHaveClass("active");
   });
 });
